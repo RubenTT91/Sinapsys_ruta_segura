f0c66ad41da1338fab54df99fae5b2ec9c4c4274
Lara IV <ruben.toro1@gmail.com>
2025-03-22 15:13:06 -0500

    Se unifica la barra de búsqueda con el header en un mismo componente

--- sinapsys-rs/src/App.js
+++ sinapsys-rs/src/App.js
@@ -1,13 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header";
-import SearchBar from "./components/searchBar";
 
 function App() {
   return (
     <div>
       <Header />
-      <SearchBar />
+      {/* <SearchBar /> */}
     </div>
   );
 }
